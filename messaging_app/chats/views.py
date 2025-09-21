73ba77aa0a16c61d89ca9fd2e1a8640924bd4592
Engineer Tony <wangolotony4@gmail.com>
2025-09-21 17:02:27 +0300

    feat: add Conversation and Message viewsets with create and list endpoints
    - Implement ConversationViewSet with support for creating conversations and listing messages
    - Implement MessageViewSet with support for sending messages to existing conversations
    - Added custom action to retrieve all messages within a conversation

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Conversation, Message, User
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        """Create a new conversation with participants"""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        conversation = serializer.save()
+        return Response(
+            ConversationSerializer(conversation).data,
+            status=status.HTTP_201_CREATED
+        )
+
+    @action(detail=True, methods=["get"])
+    def messages(self, request, pk=None):
+        """Get all messages in a conversation"""
+        conversation = self.get_object()
+        messages = conversation.messages.all()
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        """Send a message to an existing conversation"""
+        conversation_id = request.data.get("conversation")
+        sender_id = request.data.get("sender")
+
+        conversation = get_object_or_404(Conversation, id=conversation_id)
+        sender = get_object_or_404(User, id=sender_id)
+
+        message = Message.objects.create(
+            conversation=conversation,
+            sender=sender,
+            content=request.data.get("content")
+        )
+
+        serializer = self.get_serializer(message)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
