6056d56f26b97744fa6f9f87996b2777c054a697
Engineer Tony <wangolotony4@gmail.com>
2025-09-28 20:16:12 +0300

    2

--- 0x03-MessagingApp-Django/chats/middleware.py
+++ 0x03-MessagingApp-Django/chats/middleware.py
@@ -1,6 +1,7 @@
 # chats/middleware.py
 import logging
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -23,3 +24,17 @@ class RequestLoggingMiddleware:
         # Continue with normal request-response cycle
         response = self.get_response(request)
         return response
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour  # get the current server hour (0–23)
+
+        # Deny access outside 6AM–9PM
+        if current_hour < 6 or current_hour >= 21:
+            return HttpResponseForbidden("Access to the chat is restricted during this time.")
+
+        return self.get_response(request)
